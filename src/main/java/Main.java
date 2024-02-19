9e71280d53073cfcb72d807486419e4b50d265a5
Markus23D <maan0007@stud.kea.dk>
2024-02-19 13:46:38 +0100

    yep

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Create attraction :D");
+        System.out.printf("Delete attraction :>(");
     }
 }
